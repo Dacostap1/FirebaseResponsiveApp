e1cfb6fbebd10ee5478446740e3a6d18def0017c
Daniel Acosta <d4ni3L_15@hotmail.com>
2023-01-04 21:37:44 -0500

    mejorando AsyncImage

--- FirebaseResponsiveApp/presentation/View/components/CardComponent.swift
+++ FirebaseResponsiveApp/presentation/View/components/CardComponent.swift
@@ -17,23 +17,42 @@ struct CardComponent: View {
     var body: some View {
         VStack(spacing: 20){
             //FALTA APRENDER A USARLO
-            AsyncImage(url: URL(string: game.portada)) { image in
-                image
-                    .resizable()
-                          .aspectRatio(contentMode: .fill)
-                          .frame(width: 300, height: 200, alignment: .topLeading)
-                          .cornerRadius(20)
-                          .clipped()
-                
-            } placeholder: {
-                Image(systemName: "photo.fill")
-                    .resizable()
-                          .aspectRatio(contentMode: .fill)
-                          .frame(width: 300, height: 200, alignment: .center)
-                          .cornerRadius(20)
-                          .clipped()
-            }
+//            AsyncImage(url: URL(string: game.portada)) { image in
+//                image
+//                    .resizable()
+//                          .aspectRatio(contentMode: .fill)
+//                          .frame(width: 300, height: 200, alignment: .topLeading)
+//                          .cornerRadius(20)
+//                          .clipped()
+//
+//            } placeholder: {
+//                Image(systemName: "photo.fill")
+//                    .resizable()
+//                          .aspectRatio(contentMode: .fill)
+//                          .frame(width: 300, height: 200, alignment: .center)
+//                          .cornerRadius(20)
+//                          .clipped()
+//            }
 
+            AsyncImage(url: URL(string: game.portada)!) { phase in // 1
+                      if let image = phase.image { // 2
+                          // if the image is valid
+                          image
+                              .resizable()
+                              .aspectRatio(contentMode: .fill)
+                              .frame(width: 300, height: 200, alignment: .center)
+                              .cornerRadius(20)
+                              .clipped()
+                          
+                      } else if phase.error != nil { // 3
+                          // some kind of error appears
+                          Text("No image available")
+                      } else {
+                          //appears as placeholder image
+                          ProgressView().font(.largeTitle)
+                      }
+            }.frame(width: 300, height: 200, alignment: .center)
+            
 //           ImageFirebase(imageUrl: game.portada)
             Text(game.titulo)
                 .font(.subheadline)
